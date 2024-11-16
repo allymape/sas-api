b0bfed3af8a75a2a572eb5d9063236561d84cdc1
mape.ally <mape.ally@gmail.com>
2024-11-16 16:00:17 +0300

    delete duplicate

--- models/schoolModel.js
+++ models/schoolModel.js
@@ -422,7 +422,7 @@ module.exports = {
           results.forEach((result) => {
             const { id, establishing_school_id, tracking_number } = result;
             // Delete from `school_registrations`
-            console.log(tracking_number)
+            console.log(result)
             db.query(
               `DELETE FROM school_registrations WHERE id = ?`,
               [id],
