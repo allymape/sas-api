54a32091a71a49da19430ccc84d956e0e6061b9b
mape.ally <mape.ally@gmail.com>
2024-11-16 15:55:51 +0300

    delete duplicate

--- models/schoolModel.js
+++ models/schoolModel.js
@@ -418,6 +418,7 @@ module.exports = {
       HAVING COUNT(*) > 1;`,
       (error, results) => {
         if (error) return console.log(error);
+        console.log(results)
         if (results.length > 0) {
           results.forEach((result) => {
             const { id, establishing_school_id, tracking_number } = result;
