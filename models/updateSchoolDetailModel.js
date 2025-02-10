cefe43d038a79d3765341ec1c4a4ae903a6d3852
mape.ally <mape.ally@gmail.com>
2025-02-10 15:09:57 +0300

    fix

--- models/updateSchoolDetailModel.js
+++ models/updateSchoolDetailModel.js
@@ -59,7 +59,7 @@ module.exports = {
                         const combinationIds = school_combinations.map(
                           (row) => row.combination_id
                         );
-                        console.log(results);
+                        // console.log(results);
                         callback(
                           results[0],
                           owners[0],
